3e084642f8f03fe39269bfbd4ecce3bae33c45e0
Dipanshu <dipanshusingh781@gmail.com>
2026-05-23 19:03:38 +0530

    Day 6

--- day5.js
+++ day5.js
@@ -1,4 +1,8 @@
-for (let i=1; i<=10; i++){
+/*for (let i=1; i<=10; i++){
     console.log(45*i);
     console.log ("valuse of i is" +i);
+}*/
+for (let counter=10; counter<=10; counter--
+){
+    console.log("helloooo")
 }
---
+++ day6arrays.js
@@ -0,0 +1,6 @@
+let marks = [12 , 45, 67, 89, 90];
+console.log(marks[0]);
+length = marks.length;
+console.log(length);
+
+
