7be63e650ae0db01138662dbcc1b9056baaec114
Dipanshu <dipanshusingh781@gmail.com>
2026-05-25 16:41:17 -0300

    practice question

---
+++ practiceF.js
@@ -0,0 +1,6 @@
+const sum = (a, b) => a + b;
+console.log(sum(5, 10));
+const multiplyyy = (a, b)=> a*b;
+console.log(multiplyyy(5,10));
+const multiply = (a,b,c,d) => a*b*c*d;
+console.log(multiply(5,10,15,20));
