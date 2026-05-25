84f4d8b07fc556840199b80a491a4beea20b1eae
Dipanshu <dipanshusingh781@gmail.com>
2026-05-25 17:26:35 -0300

    practice question

---
+++ practiceF2.js
@@ -0,0 +1,8 @@
+//take number n and make an array
+const prompt = require('prompt-sync')();
+let n = Number(prompt("enter number"));
+let arry = [];
+for (let i=0; i<n; i++){
+    arry [i] = i;
+
+}console.log(arry); 
